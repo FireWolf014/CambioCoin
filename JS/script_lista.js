a1d716163adf49e6245a7e8405e970cde7de9afc
Leonardo M <95316458+FireWolf014@users.noreply.github.com>
2022-06-06 12:26:05 -0300

    Delete script_lista.js

--- JS/script_lista.js
+++
@@ -1,7 +0,0 @@
-var select = document.getElementById("selecionar-moeda");
-
-var opcaoTexto = select.options[select.selectedIndex].text;
-var opcaoValor = select.options[select.selectedIndex].value;
-
-console.log(opcaoTexto); 
-console.log(opcaoValor); 
